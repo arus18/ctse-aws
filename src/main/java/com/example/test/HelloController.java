265414aa9c6db4d924e4c6e301bbbf948afcf791
arus18 <31134360+arus18@users.noreply.github.com>
2024-04-20 20:55:41 +0530

    sonarcloud

--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 class HelloController {
@@ -9,5 +10,13 @@ class HelloController {
     @GetMapping("/")
     String hello() {
         return "Hello, world! change1";
+    }
+
+    @GetMapping("/test-outbound")
+    String testOutbound() {
+        String apiUrl = "https://www.google.com";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        return "Outbound request to " + apiUrl + " successful. Response: " + response;
     }
 }
