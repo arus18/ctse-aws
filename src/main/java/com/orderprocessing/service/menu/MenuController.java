032de3844ecd7a7d5573540541e6c4f8678108ff
Suran Sandeepa <74088854+SuranSandeepa@users.noreply.github.com>
2024-05-01 23:28:23 +0530

    Update MenuController.java

--- src/main/java/com/orderprocessing/service/menu/MenuController.java
+++ src/main/java/com/orderprocessing/service/menu/MenuController.java
@@ -9,24 +9,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/menu")
 public class MenuController {
+    // Autowiring MenuService for dependency injection
     @Autowired
     private MenuService menuService;
 
+    // Endpoint for adding a new menu item via POST request
     @PostMapping("/items")
     public ResponseEntity<MenuItem> addMenuItem(@RequestBody MenuItem menuItem) {
+         // Invoking the addMenuItem method of MenuService to add the new menu item
         MenuItem addedMenuItem = menuService.addMenuItem(menuItem);
+        // Returning a ResponseEntity with the added menu item and HTTP status code 201 (CREATED)
         return new ResponseEntity<>(addedMenuItem, HttpStatus.CREATED);
     }
 
+    // Endpoint for retrieving all menu items via GET request
     @GetMapping("/items")
     public ResponseEntity<List<MenuItem>> getAllMenuItems() {
+        // Invoking the getAllMenuItems method of MenuService to retrieve all menu items
         List<MenuItem> menuItems = menuService.getAllMenuItems();
+        // Returning a ResponseEntity with the list of menu items and HTTP status code 200 (OK)
         return new ResponseEntity<>(menuItems, HttpStatus.OK);
     }
 
+    // Endpoint for retrieving a menu item by its ID via GET request
     @GetMapping("/items/{id}")
     public ResponseEntity<MenuItem> getMenuItemById(@PathVariable Long id) {
+        // Invoking the getMenuItemById method of MenuService to retrieve the menu item by ID
         MenuItem menuItem = menuService.getMenuItemById(id);
+        // Checking if the menu item is found
         if (menuItem != null) {
             return new ResponseEntity<>(menuItem, HttpStatus.OK);
         } else {
